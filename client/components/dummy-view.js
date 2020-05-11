ed66787c0f357e750ab81e2239c047244c66fed3
snegina <snegina@yandex.ru>
2020-05-11 19:37:50 +0300

    added client

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
-import Head from './head'
+import Header from './headers'
 
 const Dummy = () => {
   return (
     <div>
-      <Head title="Hello" />
+      <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           This is dummy component
